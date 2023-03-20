b58ad8d207fbf632f15cee8c724a12892c5bfd1f
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-03-20 19:17:50 +0900

    two esc key positions

--- keymap.c
+++ keymap.c
@@ -52,7 +52,7 @@ enum _keymap_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // 0: Base Layer
   [_QWERTY] = LAYOUT_kbd75_esnahn(
-    KC_ESC,   XXXXXXX,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   MO(_SPECIAL),KC_PSCR,
+    KC_ESC,   KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   MO(_SPECIAL),KC_PSCR,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,               DM_PLY1,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,               DM_PLY2,
     LT(_MID_SPC, KC_LNG1),
