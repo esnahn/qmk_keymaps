940f0c12a666b37d14c80413778476a6dd71aff1
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-04-05 18:14:04 +0900

    change special keys as caps and backslash

--- keymap.c
+++ keymap.c
@@ -54,8 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_kbd75_esnahn(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX, MO(_SPECIAL), KC_PSCR,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,               KC_VOLU,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,               KC_VOLD,
-    LT(_MID_SPC, KC_LNG1),
+    KC_TAB,
+              KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  
+                                                                                                                                      LT(_CAPS, KC_BSLS),    KC_VOLD,
+    LT(_CAPS, KC_LNG1),
               KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                XXXXXXX,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,               KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,       KC_DEL,
     KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   LT(_MID_SPC, KC_F24), KC_SPC,          ALT_T(KC_HOME),CTL_T(KC_END), KC_LEFT,  KC_DOWN,     KC_RGHT
@@ -78,14 +80,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,            _______,  _______,
   //   _______,  _______,  _______,                      KC_LNG1,  _______,  _______,                      _______,  _______,            _______,  _______,  _______
   // ),
-  // caps lock/lang1 key layer
+  // caps lock/tab/backslash key layer
   [_CAPS] = LAYOUT_kbd75_esnahn(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+    _______,  KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,   KC_F24,   _______,  _______,  _______,
+    _______,  _______,  X(SUP2),  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  XP(LCORNB,LWCRNB),
+                                                                                                                            XP(RCORNB,RWCRNB), 
+                                                                                                                                      _______,            _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,            _______,  _______,
-    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  KC_LNG2,            _______,  _______,  _______
+    _______,  _______,  X(REFERM),_______,  _______,  _______,  _______,  _______,            X(MIDDOT),X(ELLIPS),_______,  _______,            _______,  _______,
+    _______,  _______,  _______,                      X(NBSP),  _______,  _______,                      _______,  KC_LNG2,            _______,  _______,  _______
   ),
   // middle key Layer
   [_MID_SPC] = LAYOUT_kbd75_esnahn(
